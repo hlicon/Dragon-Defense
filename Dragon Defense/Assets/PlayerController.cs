ce35ceaeba72d9f94c7120fa02cd2661063e5fb9
Simon Kincaid <smkincaid93@gmail.com>
2016-03-09 22:52:35 -0800

    Terrible first attempt, 1 hour's work

---
+++ Dragon Defense/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody2D rb;
+	public float speed;
+
+	public GameObject shot;
+	public Transform shotSpawn;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		
+		Vector3 movement = new Vector3 (moveHorizontal, moveVertical, 0.0f);
+
+		rb.AddForce (movement * speed);
+
+
+	}
+
+	void Update() {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+}
