a5ba96d5c4ad21ae438f1b168e99064747358c49
Hayden Licon <Hayden Licon>
2016-03-11 20:24:52 -0800

    Added click shooting

---
+++ Dragon Defense/Assets/ClickShoot.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClickShoot : MonoBehaviour {
+
+	private Vector2 mouseClickPosition;
+	private bool canShoot;
+	private int selection = 0;
+	private float power;
+	private float angle;
+
+	public Transform shotSpawn;
+	private Vector2 shotSpawnPos;
+
+	[Header("Weapons")]
+	public GameObject[] Shots;
+
+	void Start(){
+		canShoot = true;
+		shotSpawnPos = shotSpawn.transform.position;
+	}
+
+	void Update(){
+		GetSelection();
+		if(Input.GetMouseButton(0)){
+			mouseClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if(canShoot && selection >= 0){
+				CalculateShot();
+			}
+		} 
+	}
+		
+	private void CalculateShot(){
+		shotSpawnPos = shotSpawn.transform.position;
+		Debug.DrawLine(shotSpawnPos, mouseClickPosition, Color.red);
+
+		Vector2 Dir = mouseClickPosition - shotSpawnPos;
+
+		if(Dir.x > 4f)
+			Dir.x = 4f;
+		if(Dir.y > 4f)
+			Dir.y = 4f;
+
+		FireWeapon(Dir);
+		print(Dir);
+	}
+
+	private void FireWeapon(Vector2 Dir){
+		canShoot = false;
+		Vector2 shotSpawnPos = shotSpawn.position;
+		GameObject clone = (GameObject)Instantiate(Shots[selection], shotSpawnPos, Quaternion.identity);
+		ShotClass cloneShotClass = clone.GetComponent<ShotClass>();
+		clone.GetComponent<Rigidbody2D>().AddForce(Dir + Dir, ForceMode2D.Impulse);
+
+		StartCoroutine(ShotWait());
+	}
+
+	private IEnumerator ShotWait(){
+		yield return new WaitForSeconds(.5f);
+		canShoot = true;
+	}
+
+	private void GetSelection(){
+		if(Input.GetKeyDown(KeyCode.Alpha1)){
+			selection = 0;
+		}
+	}
+
+}
--- Dragon Defense/Assets/Scripts/PlayerController.cs
+++ Dragon Defense/Assets/Scripts/PlayerController.cs
@@ -36,70 +36,8 @@ public class PlayerController : MonoBehaviour {
 
 		rb.AddForce (movement * speed);
 		//Actually moving the player by adding force to the rigidbody component
-		GetShotInputs();
 	}
 
 	void Update() {
-		if(Input.GetKeyDown(KeyCode.Alpha1)){
-			shotSelection = 0;
-			//Lets the player choose their weapon
-	}
-		if (Input.GetKeyDown (KeyCode.Space) && canShoot) {
-			canShoot = false;
-			StartCoroutine(FireShot(shotSelection, Shots[shotSelection].GetComponent<ShotClass>().amountToFire));
-			//Fires the selected shot
-			if(canShoot != true)
-				StartCoroutine(ShotWait());
-		}
-	}
-
-	private IEnumerator FireShot(int currentWeapon, int amountOfShots){
-	    //Making it so the player cannot fire again
-		GameObject clone = (GameObject)Instantiate(Shots[currentWeapon], shotSpawn.position, shotSpawn.rotation);
-		//Instantiating the weapon
-		ShotClass cloneShotClass = clone.GetComponent<ShotClass>();
-		//Getting the Shot Class
-		Vector2 forceArea = new Vector2(shotSpawn.localPosition.x + power, shotSpawn.localPosition.y + angle/8.5f);
-		//Setting up where the fire the firebolt
-		clone.GetComponent<Rigidbody2D>().AddForce(forceArea, ForceMode2D.Impulse);
-		//Moving the spawned weapon in the forceArea direction
-		if(!cloneShotClass.lobShot) {
-			clone.GetComponent<Rigidbody2D>().gravityScale = 0;
-			clone.GetComponent<Rigidbody2D>().velocity *= 2;
-			//Turning off gravity in order to make it a "straight" shot
-		}
-		yield return new WaitForSeconds(.1f); //Wait's .1 of a second between shots fired
-		if(amountOfShots > 1){
-			//If the shots are > 1 we'll fire another
-			amountOfShots -= 1;
-			StartCoroutine(FireShot(currentWeapon, amountOfShots));
-		}
-	}
-
-	private void GetShotInputs(){
-		if(Input.GetKey(KeyCode.UpArrow)){
-			angle += 2f;
-			if(angle >= 45f)
-				angle = 45f;
-		} else if(Input.GetKey(KeyCode.DownArrow)){
-			angle -= 2f;
-			if(angle <= -180f)
-				angle = -180f;
-		}
-		if(Input.GetKey(KeyCode.Z)){
-			power -= .5f;
-			if(power <= 0)
-				power = 0;
-		} else if (Input.GetKey(KeyCode.X)){
-			power += .5f;
-			if(power >= 10)
-				power = 10;
-		}
-
-	}
-
-	private IEnumerator ShotWait(){
-		yield return new WaitForSeconds(.5f);
-		canShoot = true; //Allows the player to fire again after .5 of a second
 	}
 }
