c35a07113d8f0cf848af70f1b6e85683e7dbca86
Hayden Licon <Hayden Licon>
2016-03-11 20:39:21 -0800

    Forgot to remove everything from playercontroller and added comments to ClickShoot

--- Dragon Defense/Assets/ClickShoot.cs
+++ Dragon Defense/Assets/ClickShoot.cs
@@ -18,14 +18,19 @@ public class ClickShoot : MonoBehaviour {
 	void Start(){
 		canShoot = true;
 		shotSpawnPos = shotSpawn.transform.position;
+
+		Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Shot")); 
+		//Ignores the shot layer so the player doesn't get pushed around by the shots
+
 	}
 
 	void Update(){
 		GetSelection();
-		if(Input.GetMouseButton(0)){
+		if(Input.GetMouseButton(0)){ //Is the mouse button being held down?
 			mouseClickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			if(canShoot && selection >= 0){
-				CalculateShot();
+			//Setting mouse position so we don't have to call >>^^ everytime
+			if(canShoot && selection >= 0){ //Do we have a proper selection and can we shoot?
+				CalculateShot(); //If so, let's "calculate" where we're firing
 			}
 		} 
 	}
@@ -33,34 +38,42 @@ public class ClickShoot : MonoBehaviour {
 	private void CalculateShot(){
 		shotSpawnPos = shotSpawn.transform.position;
 		Debug.DrawLine(shotSpawnPos, mouseClickPosition, Color.red);
+		//Drawing a line in the scene (not the game view) to help display where we're clicking
 
 		Vector2 Dir = mouseClickPosition - shotSpawnPos;
+		//Where we want to apply the force to
 
 		if(Dir.x > 4f)
 			Dir.x = 4f;
 		if(Dir.y > 4f)
 			Dir.y = 4f;
+		//Limit the distance so we don't fire too far
 
-		FireWeapon(Dir);
-		print(Dir);
+		FireWeapon(Dir); //After we've "calculated" the distance we're going to fire the shot
 	}
 
 	private void FireWeapon(Vector2 Dir){
 		canShoot = false;
-		Vector2 shotSpawnPos = shotSpawn.position;
 		GameObject clone = (GameObject)Instantiate(Shots[selection], shotSpawnPos, Quaternion.identity);
+		//Spawn the shot from the selection
 		ShotClass cloneShotClass = clone.GetComponent<ShotClass>();
+		//Getting the shot class
 		clone.GetComponent<Rigidbody2D>().AddForce(Dir + Dir, ForceMode2D.Impulse);
+		//Applying the force we passed in from calculating
+
+		if(!cloneShotClass.lobShot){
+			clone.GetComponent<Rigidbody2D>().gravityScale = 0; //Turn gravity off for straight shots
+		}
 
 		StartCoroutine(ShotWait());
 	}
 
 	private IEnumerator ShotWait(){
-		yield return new WaitForSeconds(.5f);
+		yield return new WaitForSeconds(.5f); //Wait half a second before we can shoot again
 		canShoot = true;
 	}
 
-	private void GetSelection(){
+	private void GetSelection(){ //Setting selection to the selected shot in the Shots array
 		if(Input.GetKeyDown(KeyCode.Alpha1)){
 			selection = 0;
 		}
--- Dragon Defense/Assets/Scripts/PlayerController.cs
+++ Dragon Defense/Assets/Scripts/PlayerController.cs
@@ -5,25 +5,12 @@ public class PlayerController : MonoBehaviour {
 
 	[Header("Player Values")]
 	public float speed; //Speed of the player
-	public Transform shotSpawn; //Spawn point of shots (mouth of dragon)
-	public float angle;
-	public float power;
-
-	[Header("Weapons")]
-	public GameObject[] Shots; //Array of GameObjects (weapons to spawn)
-
 	private Rigidbody2D rb; //Rigidbody component
-	private int shotSelection; //Which shot is currently selected
-	private bool canShoot; //Tells us whether the player can shoot or not.
 
 	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 
-		canShoot = true;
-
-		Physics2D.IgnoreLayerCollision(gameObject.layer, LayerMask.NameToLayer("Shot")); 
-		//Ignores the shot layer so the player doesn't get pushed around by the shots
 	}
 
 	void FixedUpdate () {
