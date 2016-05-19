aa4375e38467f5572405fca1dc29c6b464d17e43
Hayden Licon <Hayden Licon>
2016-05-19 10:12:50 -0700

    Upgrade UI (not finished), new background images

--- Dragon Defense/Assets/Scripts/Loot Scripts/LootManager.cs
+++ Dragon Defense/Assets/Scripts/Loot Scripts/LootManager.cs
@@ -4,16 +4,20 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class LootManager : MonoBehaviour {
-	public RectTransform panel;
+
+	[Header("Sell Menu Panel")]
+	[SerializeField] private GameObject contentPanel;
+	private RectTransform contentPanelRect;
+
+	[Header("Loot Prefab")]
+	[SerializeField] private GameObject loot;
+
+	[Header("Scroll Bar Stuff")]
+	[SerializeField] private ContentSizeFitter menuFitter;
+	[SerializeField] private GameObject menuScrollbar;
 
 	static private Stack<GameObject> lootList;
 	static private GameObject[] lootTypeList;
-
-	[SerializeField] private GameObject loot;
-	[SerializeField] private GameObject contentPanel;
-
-	[SerializeField] private ContentSizeFitter menuFitter;
-	[SerializeField] private GameObject menuScrollbar;
 
 	const float LOOT_CHANCE = 60; //will alter later
 	static Vector2 lootSpawn = new Vector2(9999, 9999);
@@ -44,6 +48,7 @@ public class LootManager : MonoBehaviour {
 	void Start () {
 		lootList = new Stack<GameObject>();
 		lootTypeList = new GameObject[Loot.LootTypeList.Length];
+		contentPanelRect = contentPanel.GetComponent<RectTransform>();
 
 		for (int i = 0; i < Loot.LootTypeList.Length; ++i) {
 			lootTypeList [i] = Instantiate (loot, new Vector2 (99999, 99999), Quaternion.identity) as GameObject;
@@ -89,7 +94,7 @@ public class LootManager : MonoBehaviour {
 			temp.SetLootName (lootName);
 			temp.SetLootVal (lootVal);
 
-			nextItem.transform.SetParent (panel, false);
+			nextItem.transform.SetParent (contentPanelRect, false);
 		}
 	}
 
